3cee3a717bf59b7c89c36eea48b831c28d47662e
srichandj123 <srichandbec@gmail.com>
2024-07-03 00:43:07 +0530

    subnetsvnetstest

--- main.tf
+++ main.tf
@@ -20,3 +20,31 @@ resource "random_string" "random" {
   special = false
   upper   = false
 }
+
+data "azurerm_client_config" "current" {}
+#Keyvault
+
+resource "azurerm_key_vault" "kv" {
+  name                = "jakk-key-vault"
+  location            = module.rg-keyvault.location
+  resource_group_name = module.rg-keyvault.rg_name
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  tags                = local.tags
+  dynamic "access_policy" {
+    for_each = var.settings
+    content {
+      tenant_id       = data.azurerm_client_config.current.tenant_id
+      object_id       = access_policy.value["objid"]
+      key_permissions = access_policy.value["kperms"]
+
+      secret_permissions = [
+        "Get", "List", "Set"
+      ]
+
+      storage_permissions = [
+        "Get", "List", "Set"
+      ]
+    }
+  }
+}
---
+++ networking.tf
@@ -0,0 +1,33 @@
+#Storageaccount
+resource "azurerm_storage_account" "stgkv" {
+  name                          = local.stgname
+  resource_group_name           = azurerm_key_vault.kv.resource_group_name
+  location                      = azurerm_key_vault.kv.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  public_network_access_enabled = false
+  network_rules {
+    default_action             = "allow"
+    virtual_network_subnet_ids = [azurerm_subnet.snet1[0]]
+  }
+  tags = local.tags
+}
+
+#vnet
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "VNET-1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_key_vault.kv.location
+  resource_group_name = azurerm_key_vault.kv.resource_group_name
+  tags                = local.tags
+}
+
+#Subnet
+resource "azurerm_subnet" "snet1" {
+  count                = 2
+  name                 = "subnet-${count.index}"
+  resource_group_name  = azurerm_key_vault.kv.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.${count.index}+1.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
